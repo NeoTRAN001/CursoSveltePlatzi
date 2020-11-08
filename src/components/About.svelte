10e25474dacc5fa7404d7a495f44c9fce0af6b58
NeoTRAN001 <neo_tran@outlook.com>
2020-11-08 00:17:20 -0600

    Create About Component

---
+++ src/components/About.svelte
@@ -0,0 +1,14 @@
+<script>
+    let someText = `Frontend Developer at Platzi`;
+</script>
+
+<main>
+    <p>{someText}</p>
+</main>
+
+<style>
+    p {
+        color: var(--theme-color);
+        font-size: 20px;
+    }
+</style>
