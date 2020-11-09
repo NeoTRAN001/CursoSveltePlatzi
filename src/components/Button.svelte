4bda2a5f70bcafa1080ad8a0e08c319ea8d1a34a
NeoTRAN001 <neo_tran@outlook.com>
2020-11-08 22:55:16 -0600

    Fetch, Binding and Events

---
+++ src/components/Button.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let count = 0;
+    export let text;
+</script>
+
+<main>
+    <button on:click>{ text } { count === 0 ? '' : count }</button>
+</main>
---
+++ src/components/Input.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let name = "World";
+</script>
+
+<main>
+    <input type="text" bind:value="{ name }">
+    <p>Hello { name }</p>
+</main>
